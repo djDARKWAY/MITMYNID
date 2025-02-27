e15dbad1681f0c7e7ebc5501fd31e925f650f271
djDARKWAY <8230465@estg.ipp.pt>
2025-02-27 16:15:12 +0000

    Permissões de administrador no painel vertical

--- manager/src/components/layout/menu.tsx
+++ manager/src/components/layout/menu.tsx
@@ -30,8 +30,8 @@ import {
   HistoryToggleOffRounded,
   HowToRegRounded,
   PeopleAltRounded,
-  SecurityRounded
-} from '@mui/icons-material';
+  SecurityRounded,
+} from "@mui/icons-material";
 import {
   useTranslate,
   MenuItemLink,
@@ -42,62 +42,92 @@ import {
 } from "react-admin";
 import SubMenu from "./submenu";
 import { Box, Divider } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-type MenuName = 'menuSeguranca' | "menuAccessPoint" | "menuMonitorizacao" | "menuCertificados" | 'menuConfig';
+type MenuName =
+  | "menuSeguranca"
+  | "menuAccessPoint"
+  | "menuMonitorizacao"
+  | "menuCertificados"
+  | "menuConfig";
 
 const MyMenu = ({ dense = false }: MenuProps) => {
-
-  const logoBigImg = useRef<string>('MMN_H_RGB.svg');
-  const logoSmallImg = useRef<string>('MMN_V_RGB.svg');
+  const logoBigImg = useRef<string>("MMN_H_RGB.svg");
+  const logoSmallImg = useRef<string>("MMN_V_RGB.svg");
   const translate = useTranslate();
   const { permissions, isLoading } = usePermissions();
   //const { identity : profile, isLoading : isLoadingProfile } = useGetIdentity();
-  const [resource] = useStore('resource.name');
+  const [resource] = useStore("resource.name");
   const [open] = useSidebarState();
 
   const [state, setState] = useState({
-    menuSeguranca: resource === 'menuSeguranca' ? true : false,
-    menuAccessPoint: resource === 'menuAccessPoint' ? true : false,
-    menuCertificados: resource === 'menuConfig' ? true : false,
-    menuMonitorizacao: resource === 'menuConfig' ? true : false,
-    menuConfig: resource === 'menuConfig' ? true : false
+    menuSeguranca: resource === "menuSeguranca" ? true : false,
+    menuAccessPoint: resource === "menuAccessPoint" ? true : false,
+    menuCertificados: resource === "menuConfig" ? true : false,
+    menuMonitorizacao: resource === "menuConfig" ? true : false,
+    menuConfig: resource === "menuConfig" ? true : false,
   });
 
-
   const handleToggle = (menu: MenuName) => {
-    setState(state => ({ ...state, [menu]: !state[menu] }));
+    setState((state) => ({ ...state, [menu]: !state[menu] }));
   };
 
   return !isLoading ? (
-    <Box sx={{
-      width: theme => open ? theme.sidebar.width : theme.sidebar.closedWidth,
-      marginTop: 1,
-      marginBottom: 1,
-      transition: theme => theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    }}
+    <Box
+      sx={{
+        width: (theme) =>
+          open ? theme.sidebar.width : theme.sidebar.closedWidth,
+        marginTop: 1,
+        marginBottom: 1,
+        transition: (theme) =>
+          theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+      }}
     >
-      <Box sx={{ padding: '10px 0px 10px 0px' }}>
+      <Box sx={{ padding: "10px 0px 10px 0px" }}>
         <Link to={"/"}>
-          {open ?
-            <Box sx={{ padding: '0px 13px 0px 13px' }}>
-              {logoBigImg.current ? <img src={logoBigImg.current} alt="logo" style={{ maxWidth: '220px' }} /> : <></>}
+          {open ? (
+            <Box sx={{ padding: "0px 13px 0px 13px" }}>
+              {logoBigImg.current ? (
+                <img
+                  src={logoBigImg.current}
+                  alt="logo"
+                  style={{ maxWidth: "220px" }}
+                />
+              ) : (
+                <></>
+              )}
             </Box>
-            :
-            <Box sx={{ maxWidth: '80px', display: 'flex', justifyContent: 'center' }}>
-              {logoSmallImg.current ? <img src={logoSmallImg.current} alt="logo" style={{ width: '70%', height: '100%', marginLeft: '6px' }} /> : <></>}
+          ) : (
+            <Box
+              sx={{
+                maxWidth: "80px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              {logoSmallImg.current ? (
+                <img
+                  src={logoSmallImg.current}
+                  alt="logo"
+                  style={{ width: "70%", height: "100%", marginLeft: "6px" }}
+                />
+              ) : (
+                <></>
+              )}
             </Box>
-          }
+          )}
         </Link>
       </Box>
       <MenuItemLink
         to={{ pathname: "/dashboard" }}
-        className={"sidebar " + (open ? 'open' : 'close')}
+        className={"sidebar " + (open ? "open" : "close")}
         primaryText={translate(`ra.page.dashboard`)}
-        sx={{ color: theme => !open ? theme.sidebar.background : 'default' }}
+        sx={{
+          color: (theme) => (!open ? theme.sidebar.background : "default"),
+        }}
         leftIcon={<Insights />}
         dense={dense}
       />
@@ -190,14 +220,16 @@ const MyMenu = ({ dense = false }: MenuProps) => {
             dense={dense}
           />
 
-          <MenuItemLink
-            to={{ pathname: "/." }}
-            className={"submenuItem " + (open ? "open" : "close")}
-            primaryText={translate(`resources.certificates.validations`)}
-            sx={{ color: !open ? "transparent" : "default" }}
-            leftIcon={<CreditScoreRounded />}
-            dense={dense}
-          />
+          {permissions.includes("ADMIN") && (
+            <MenuItemLink
+              to={{ pathname: "/." }}
+              className={"submenuItem " + (open ? "open" : "close")}
+              primaryText={translate(`resources.certificates.validations`)}
+              sx={{ color: !open ? "transparent" : "default" }}
+              leftIcon={<CreditScoreRounded />}
+              dense={dense}
+            />
+          )}
         </SubMenu>
       )}
 
@@ -217,15 +249,16 @@ const MyMenu = ({ dense = false }: MenuProps) => {
             leftIcon={<WifiTetheringErrorRounded />}
             dense={dense}
           />
-
-          <MenuItemLink
-            to={{ pathname: "/." }}
-            className={"submenuItem " + (open ? "open" : "close")}
-            primaryText={translate(`resources.monitoring.logs`)}
-            sx={{ color: !open ? "transparent" : "default" }}
-            leftIcon={<HistoryToggleOffRounded />}
-            dense={dense}
-          />
+          {permissions.includes("ADMIN") && (
+            <MenuItemLink
+              to={{ pathname: "/." }}
+              className={"submenuItem " + (open ? "open" : "close")}
+              primaryText={translate(`resources.monitoring.logs`)}
+              sx={{ color: !open ? "transparent" : "default" }}
+              leftIcon={<HistoryToggleOffRounded />}
+              dense={dense}
+            />
+          )}
         </SubMenu>
       )}
     </Box>
