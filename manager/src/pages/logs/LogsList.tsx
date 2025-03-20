9cdb9bdc0ae11814baadd1c094660af0d53d92a0
djDARKWAY <8230465@estg.ipp.pt>
2025-03-20 18:39:52 +0000

    Ligeiros ajustes na tabela de Logs

--- manager/src/pages/logs/LogsList.tsx
+++ manager/src/pages/logs/LogsList.tsx
@@ -1,4 +1,4 @@
-import { List, Datagrid, TextField, FunctionField, SimpleList, usePermissions, DateField } from "react-admin";
+import { List, DatagridConfigurable , TextField, FunctionField, SimpleList, usePermissions, DateField } from "react-admin";
 import * as Icon from '@mui/icons-material'; // Import all Material-UI icons
 import { useTheme } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -30,29 +30,35 @@ export const LogsList = () => {
                     linkType={"edit"}
                 />
             ) : (
-                <Datagrid rowClick="show" bulkActionButtons={false} >
+                <DatagridConfigurable rowClick="show" bulkActionButtons={false} >
                     <FunctionField
-                        // label="resources.logs.fields.category"
                         render={record => {
                             const logType = record.type;
                             const IconComponent = logType?.icon && Icon[logType.icon as keyof typeof Icon] ? Icon[logType.icon as keyof typeof Icon] : null;
                             
                             return (
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <div style={{ display: 'flex', alignItems: 'center', width: '1px' }}>
                                     {IconComponent && <IconComponent style={{ marginRight: '8px' }} />}
                                 </div>
                             );
                         }}
                     />
-                    <FunctionField label="resources.logs.fields.timestamp" render={record => {
-                        return new Date(record.timestamp).toLocaleString('pt-PT', { 
-                            day: '2-digit', month: '2-digit', year: 'numeric', 
-                            hour: '2-digit', minute: '2-digit', second: '2-digit', 
-                            fractionalSecondDigits: 3 
-                        });
-                    }} />
+                    <FunctionField 
+                        label="resources.logs.fields.timestamp"  
+                        render={record => {
+                            return (
+                                <div style={{ width: '1px', whiteSpace: 'nowrap' }}>
+                                    {new Date(record.timestamp).toLocaleString('pt-PT', { 
+                                        day: '2-digit', month: '2-digit', year: 'numeric', 
+                                        hour: '2-digit', minute: '2-digit', second: '2-digit', 
+                                        fractionalSecondDigits: 3 
+                                    })}
+                                </div>
+                            );
+                        }} 
+                    />
                     <TextField source="message" label="resources.logs.fields.message" />
-                </Datagrid>
+                </DatagridConfigurable >
             )}
         </List>
     );
