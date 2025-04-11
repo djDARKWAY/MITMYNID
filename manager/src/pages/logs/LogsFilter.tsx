47dd1e4cd9298fbd2de6e88c6d6da5348a0e8fef
djDARKWAY <8230465@estg.ipp.pt>
2025-04-11 20:30:04 +0100

    Hotfix: Correção das traduções serem implementadas no SelectInput das logs

--- manager/src/pages/logs/LogsFilter.tsx
+++ manager/src/pages/logs/LogsFilter.tsx
@@ -1,9 +1,11 @@
 import { TextInput, SelectInput, DateInput } from "react-admin";
 import { useEffect, useState } from "react";
 import { fetchUtils } from "ra-core";
+import { useTranslate } from "react-admin";
 
 export const LogsFilters = (permissions: string[]) => {
     const [logTypes, setLogTypes] = useState<{ id: number; type: string }[]>([]);
+    const translate = useTranslate();
 
     useEffect(() => {
         const fetchLogTypes = async () => {
@@ -28,7 +30,18 @@ export const LogsFilters = (permissions: string[]) => {
     
 
     const filters = [
-        <SelectInput key="type_id" source="type_id" label="pos.logs.type" choices={logTypes.map(logType => ({ id: logType.id, name: logType.type }))} fullWidth alwaysOn resettable />,
+        <SelectInput 
+            key="type_id" 
+            source="type_id" 
+            label="pos.logs.type" 
+            choices={logTypes.map(logType => ({ 
+                id: logType.id, 
+                name: translate(`show.dashboard.log_type.${logType.type.toLowerCase()}`) 
+            }))} 
+            fullWidth 
+            alwaysOn 
+            resettable 
+        />,
         <DateInput key="timestamp" source="timestamp" label="pos.logs.timestamp" fullWidth/>,
     ];
 
